437fd5c2bdb1290b32f8233af7dff8409e99b394
Luisa Forero <fore0045@algonquinlive.com>
2023-01-11 14:27:51 -0500

    Includes JS for product cards container

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const burguerMenuIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const shoppingCartMenuIcon = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-details')
+const aside = document.querySelector('.product-details');
+const cardsContainer = document.querySelector('.cards-container');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -34,3 +35,58 @@ function toggleShoppingCartAside() {
     aside.classList.toggle('inactive');
 
 }
+
+const productList = [];
+
+productList.push({
+    name: 'Hand made bag',
+    price: 220,
+    image: './images/accesories/bag.jpg',
+});
+
+productList.push({
+    name: 'clock',
+    price: 70,
+    image: './images/accesories/bag.jpg',
+});
+
+function renderProducts(arr) {
+    for (product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+        const productImg = document.createElement('img');
+        // modify the attribute src by inserting the value of image from the array
+        productImg.setAttribute('src', product.image);
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const productInfoDiv = document.createElement('div');
+
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/add_cart.svg');
+
+        productInfoFigure.appendChild(productImgCart);
+
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+
+        cardsContainer.appendChild(productCard);
+    };
+}
+
+renderProducts(productList);
